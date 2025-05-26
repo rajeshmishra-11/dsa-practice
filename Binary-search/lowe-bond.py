6e22cb226d87d034147df23a83b51f996308d45a
rajesh mishra <rajeshmishra847410@gmail.com>
2025-05-26 11:55:48 +0530

    find the lower bond

---
+++ Binary-search/lowe-bond.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lowerBound(self, arr, target):
+        n=len(arr)
+        ans=n
+        low=0
+        high=n-1
+        while low<=high:
+            mid=(low+high)//2
+            
+            if arr[mid]>=target:
+                ans=mid
+                high=mid-1
+            
+            else:
+                low=mid+1
+        
+        return ans
